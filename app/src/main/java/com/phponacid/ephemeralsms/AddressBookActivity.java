64992f01705e28de086ad54aa8691d8312341ffb
Christopher Pedersen <chrispedatx@gmail.com>
2017-11-27 19:58:37 -0600

    added AddressBookActivity

---
+++ app/src/main/java/com/phponacid/ephemeralsms/AddressBookActivity.java
@@ -0,0 +1,70 @@
+package com.phponacid.ephemeralsms;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class AddressBookActivity extends AppCompatActivity {
+
+
+    String urlExtraAddressBookActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_book);
+
+        urlExtraAddressBookActivity = getIntent().getStringExtra("URL_EXTRA");
+
+
+        String phoneNumber = null;
+
+        Uri CONTENT_URI;
+        CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+
+        ArrayList<Contact> contactsArray = new ArrayList<Contact>();
+
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                Contact currentContact = new Contact();
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+                if (hasPhoneNumber > 0) {
+                    // contactsArray[contactsArrayIndex].setName(name);
+                    currentContact.setName(name);
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        // contactsArray[contactsArrayIndex].setPhoneNumber(phoneNumber);
+                        currentContact.setPhoneNumber(phoneNumber);
+                    }
+                    phoneCursor.close();
+                }
+                contactsArray.add(currentContact);
+            }
+        }
+
+        ContactAdapter adapter = new ContactAdapter(this, contactsArray, urlExtraAddressBookActivity);
+        ListView listView = (ListView) findViewById(R.id.activity_address_book);
+        listView.setAdapter(adapter);
+    }
+}
